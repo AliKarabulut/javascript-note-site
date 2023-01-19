1362a468af1d6a7548478353b518b6b12434d076
Ali Karabulut <a.kbt6666@gmail.com>
2023-01-19 21:00:38 +0300

    searchbar ve setinterval

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,31 +1,40 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./Search.module.css";
 import SearchBar from "./Searchbar";
 
 const Search = (props) => {
   const [inputVal, setInputVal] = useState("");
   const [isFocused, setIsFocused] = useState(false);
-
+  const addToInputVal = (props) =>{
+    setIsFocused(false)
+    setInputVal(props)
+  }
+  
   return (
     <div className={styles.wrapper}>
       <div className={`${styles.inputwrapper} ${isFocused ? styles.borderHalf : styles.borderFull}`}>
         <input
           onChange={(e) => {
             setInputVal(e.target.value);
+            
           }}
           onFocus={() => {
             setIsFocused(true);
           }}
           onBlur={() => {
-            setIsFocused(false);
+            setTimeout(() => {
+              setIsFocused(false)
+            }, 100);
+            
           }}
           className={styles.search}
+          value={inputVal}
           type="text"
           placeholder="Ara"
         />
         <i className={styles.icon}></i>
       </div>
-      <SearchBar send={inputVal} />
+      {isFocused && <SearchBar send={inputVal} onInputVal={addToInputVal}/>}
     </div>
   );
 };
--- src/Search/Searchbar.js
+++ src/Search/Searchbar.js
@@ -1,18 +1,37 @@
 import String from "../Metotlar/StringMetot";
 import Number from "../Metotlar/NumberMetot";
 import Array from "../Metotlar/ArrayMetot";
-import styles from './Searchbar.module.css'
+import styles from "./Searchbar.module.css";
 const SearchBar = (props) => {
   return (
     <div className={styles.listWrapper}>
-      {String.filter((val) => val.id.toLowerCase().includes(props.send))
-      .map((val, key) => {
-        return (
-          <div className={styles.list} key={key}>
-            <p>{val.id}</p>
-          </div>
-        );
-      })}
+      {String.filter((val) => val.id.toLowerCase().includes(props.send.toLowerCase())).slice(0,3).map(
+        (val, key) => {
+          return (
+            <div onClick={()=> props.onInputVal(val.id)} className={styles.list} key={key} >
+              {val.id}
+            </div>
+          );
+        }
+      )}
+      {Number.filter((val) => val.id.toLowerCase().includes(props.send.toLowerCase())).slice(0,3).map(
+        (val, key) => {
+          return (
+            <div onClick={()=> props.onInputVal(val.id)} className={styles.list} key={key} >
+              {val.id}
+            </div>
+          );
+        }
+      )}
+      {Array.filter((val) => val.id.toLowerCase().includes(props.send.toLowerCase())).slice(0,3).map(
+        (val, key) => {
+          return (
+            <div onClick={()=> props.onInputVal(val.id)} className={styles.list} key={key} >
+              {val.id}
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
