0ae81c16a8acc5401a898ae944376aa31feccf10
Ali Karabulut <a.kbt6666@gmail.com>
2023-02-24 13:15:42 +0300

    searchbarr arrow key 1

--- src/Search/Searchbar.js
+++ src/Search/Searchbar.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { metotActions } from "../store/store";
+import { useEffect, useState } from "react";
 import String from "../Metotlar/StringMetot";
 import Number from "../Metotlar/NumberMetot";
 import Array from "../Metotlar/ArrayMetot";
@@ -7,21 +8,45 @@ import styles from "./Searchbar.module.css";
 
 const SearchBar = (props) => {
   const dispatch = useDispatch();
+  const [selected, setSelected] = useState(0);
 
   const barClickHandler = (e) => {
-    console.log(e);
     dispatch(metotActions.addmetot(e));
     dispatch(metotActions.addMetotHeader(e.name));
     props.onInputVal(e.id);
   };
 
-  const { send } = props;
-  const sendLower = send.toLowerCase();
+  const sendLower = props.send.toLowerCase();
   const filteredArray = [].concat(
     String.filter((val) => val.id.toLowerCase().includes(sendLower)),
     Number.filter((val) => val.id.toLowerCase().includes(sendLower)),
     Array.filter((val) => val.id.toLowerCase().includes(sendLower))
   );
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        setSelected((prev) => (prev > 0 ? prev - 1 : prev));
+        break;
+      case "ArrowDown":
+        setSelected((prev) =>
+          prev < filteredArray.length - 1 && prev < 4 ? prev + 1 : prev
+        );
+        break;
+      case "Enter":
+        barClickHandler(filteredArray[selected]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
 
   return (
     <div className={styles.listWrapper}>
@@ -31,7 +56,7 @@ const SearchBar = (props) => {
           components.push(
             <div
               onClick={barClickHandler.bind(null, filteredArray[i])}
-              className={styles.list}
+              className={`${styles.list} ${i === selected && styles.selected}`}
               key={i}
             >
               {filteredArray[i].id}
